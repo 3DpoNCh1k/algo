eb9bd545cc5a60dee8d347c55ab80cd81056b729
Poliashov Maksim <polyashov17@ya.ru>
2024-02-09 22:36:45 +0400

    add file for experiments

---
+++ tests/run.cpp
@@ -0,0 +1,17 @@
+#include <cstddef>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include "../header.hpp"
+
+// NOLINTBEGIN
+using namespace std;
+
+// NOLINTEND
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "Hello" << endl;
+}
