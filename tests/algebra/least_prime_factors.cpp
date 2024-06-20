81d9d19b97e00f9e0749ea6405b293512c709c14
Polyashov Maxim <polyashovmaxim@gmail.com>
2024-06-20 12:23:54 +0300

    fix tests

--- tests/algebra/least_prime_factors.cpp
+++ tests/algebra/least_prime_factors.cpp
@@ -1,24 +1,26 @@
-#include "../../header.hpp"
+#include <iostream>
+#include <vector>
+
 #include "../../algebra/algebra.hpp"
 
 // https://codeforces.com/group/CYMPFXi8zA/contest/279284/problem/B
 void Solve() {
-  vector<int> least_prime_factors = LeastPrimeFactors(2e6 + 5);
+  std::vector<int> least_prime_factors = LeastPrimeFactors(2e6 + 5);
   int n;
-  cin >> n;
+  std::cin >> n;
   for (int i = 0; i < n; ++i) {
     int a;
-    cin >> a;
+    std::cin >> a;
     while (a > 1) {
-      cout << least_prime_factors[a] << " ";
+      std::cout << least_prime_factors[a] << " ";
       a /= least_prime_factors[a];
     }
-    cout << "\n";
+    std::cout << "\n";
   }
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
   Solve();
 }
