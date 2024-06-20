f613d7562ec91c81ea8d3c9dfc4a7de2eaac5091
Polyashov Maxim <polyashovmaxim@gmail.com>
2024-06-20 16:54:50 +0300

    remove prefix requirements

--- algo/algebra/constants.hpp
+++ algo/algebra/constants.hpp
@@ -1,8 +1,8 @@
 #include <algo/utils/types.hpp>
 
 namespace algo::algebra {
-// NOLINTBEGIN
+using utils::i32;
+using utils::i64;
 const i64 INF64 = 1e18;
 const i32 INF32 = 1e9;
-// NOLINTEND
 }  // namespace algo::algebra
