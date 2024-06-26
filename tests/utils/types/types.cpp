aa3b42e91418329acf2ac161012c4d22a8b5b63f
Polyashov Maxim <polyashovmaxim@gmail.com>
2024-06-26 21:58:15 +0300

    better

--- tests/utils/types/types.cpp
+++ tests/utils/types/types.cpp
@@ -1,19 +1,13 @@
 #include <algo/utils/types/128_t.hpp>
-#include <algo/utils/types/types.hpp>
 #include <iostream>
 
 int main() {
   int T;
   std::cin >> T;
-  for(int t = 0; t < T; ++t) {
+  for (int t = 0; t < T; ++t) {
     using algo::utils::i128;
     i128 a, b;
     std::cin >> a >> b;
-    // auto c = a + b;
-    // std::cout << c << std::endl;
     std::cout << (a + b) << std::endl;
-    // operator<<(std::cout, c);
-    // algo::utils::operators::operator<<(std::cout, c);
-    // algo::utils::operators::operator<<(std::cout, a + b);
   }
 }
