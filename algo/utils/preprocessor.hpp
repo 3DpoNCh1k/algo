8d9345b1b033b20c9a400462da9f1cbf0a59e985
Polyashov Maxim <polyashovmaxim@gmail.com>
2024-07-07 16:37:47 +0300

    add some defines

--- algo/utils/preprocessor.hpp
+++ algo/utils/preprocessor.hpp
@@ -1,1 +1,11 @@
 #pragma once
+
+#define AS_SINGLE_ARGUMENT(...) __VA_ARGS__
+
+#define _AS_STRING_EXPANDED(input) #input
+
+#define AS_STRING(input) _AS_STRING_EXPANDED(AS_SINGLE_ARGUMENT(input))
+
+#define _CONCAT_EXPANDED(x, y) x##y
+#define CONCAT(x, y) \
+  _CONCAT_EXPANDED(AS_SINGLE_ARGUMENT(x), AS_SINGLE_ARGUMENT(y))
