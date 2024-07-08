9e8222ea657a06c4f53e8156e75a2e7a13dd166c
Polyashov Maxim <polyashovmaxim@gmail.com>
2024-07-08 15:10:34 +0300

    add modular type

---
+++ algo/utils/types/modular.hpp
@@ -0,0 +1,121 @@
+#include <type_traits>
+
+#include <algo/utils/types/types.hpp>
+
+namespace algo::utils {
+template <typename InternalType, InternalType Mod,
+          typename = std::enable_if_t<std::is_integral_v<InternalType> &&
+                                      std::is_signed_v<InternalType>>>
+struct Modular {
+  static constexpr InternalType MOD = Mod;
+  InternalType value = 0;
+
+  Modular(){};
+  template <typename T,
+            typename = std::enable_if_t<std::is_integral_v<T> &&
+                                        std::is_signed_v<InternalType>>>
+  // NOLINTNEXTLINE
+  Modular(T v) {
+    v %= MOD;
+    if (v < 0) {
+      v += MOD;
+    }
+    value = v;
+  }
+  Modular(const Modular&) = default;
+  Modular(Modular&&) = default;
+  Modular& operator=(const Modular&) = default;
+  Modular& operator=(Modular&&) = default;
+
+  // +
+  Modular operator+(const Modular& that) const {
+    return (value + that.value) % MOD;
+  }
+  Modular& operator+=(const Modular& that) {
+    *this = *this + that;
+    return *this;
+  }
+  // +
+
+  // Unary -
+  Modular operator-() const {
+    return value == 0 ? 0 : MOD - value;
+  }
+  // Unary -
+
+  // -
+  Modular operator-(const Modular& that) const {
+    return *this + (-that);
+  }
+  Modular& operator-=(const Modular& that) {
+    *this = *this - that;
+    return *this;
+  }
+  // -
+
+  // *
+  Modular operator*(const Modular& that) const {
+    return (value * that.value) % MOD;
+  }
+
+  Modular& operator*=(const Modular& that) {
+    *this = *this * that;
+    return *this;
+  }
+  // *
+
+  // TODO: operator/
+
+  bool operator==(const Modular& that) const {
+    return value == that.value;
+  }
+
+  bool operator!=(const Modular& that) const {
+    return !(value == that.value);
+  }
+
+  bool IsValid() const {
+    return value >= 0 && value < MOD;
+  }
+
+  InternalType GetValue() {
+    return value;
+  }
+};
+
+template <typename T, typename ModularInternalType, ModularInternalType Mod>
+Modular<ModularInternalType, Mod> operator+(
+    const T& that, const Modular<ModularInternalType, Mod>& modular) {
+  return modular + that;
+}
+
+template <typename T, typename ModularInternalType, ModularInternalType Mod>
+Modular<ModularInternalType, Mod> operator-(
+    const T& that, const Modular<ModularInternalType, Mod>& modular) {
+  return Modular<ModularInternalType, Mod>(that) - modular;
+}
+
+template <typename T, typename ModularInternalType, ModularInternalType Mod>
+Modular<ModularInternalType, Mod> operator*(
+    const T& that, const Modular<ModularInternalType, Mod>& modular) {
+  return modular * that;
+}
+
+template <typename T, typename ModularInternalType, ModularInternalType Mod>
+bool operator==(const T& that,
+                const Modular<ModularInternalType, Mod>& modular) {
+  return modular == that;
+}
+
+template <typename T, typename ModularInternalType, ModularInternalType Mod>
+bool operator!=(const T& that,
+                const Modular<ModularInternalType, Mod>& modular) {
+  return modular != that;
+}
+
+// NOLINTBEGIN
+using Modular_2 = Modular<i64, 2>;
+using Modular_1_000_000_007 = Modular<i64, 1'000'000'007>;
+using Modular_1_000_000_009 = Modular<i64, 1'000'000'009>;
+// NOLINTEND
+}  // namespace algo::utils
---
+++ tests/utils/types/modular/modular.cpp
@@ -0,0 +1,177 @@
+#include <algo/utils/types/modular.hpp>
+#include <algo/utils/types/types.hpp>
+#include <tests/testing/asserts.hpp>
+
+using namespace algo::utils;
+
+
+template <typename InternalType, InternalType Mod>
+struct Tester {
+  using ModularType = Modular<InternalType, Mod>;
+
+  static constexpr i64 MOD = Mod;
+  static const i64 MinValue = -10;
+  static const i64 MaxValue = 10;
+
+  void TestCreation() {
+    {
+      ModularType value;
+      ASSERT_TRUE(value.IsValid());
+      ASSERT_TRUE(value == 0);
+      ASSERT_TRUE(value.GetValue() == 0);
+    }
+    for (i64 i = MinValue; i <= MaxValue; ++i) {
+      auto remainder = ((i % MOD) + MOD) % MOD;
+      auto value = ModularType(i);
+      ASSERT_TRUE(value.IsValid());
+      ASSERT_TRUE(value == remainder);
+      ASSERT_TRUE(value.GetValue() == remainder);
+    }
+  }
+
+  void TestAddition() {
+    for (i64 first = MinValue; first <= MaxValue; ++first) {
+      for (i64 second = MinValue; second <= MaxValue; ++second) {
+        auto remainder = (((first + second) % MOD) + MOD) % MOD;
+        {
+          auto value = ModularType(first) + second;
+          ASSERT_TRUE(value.IsValid());
+          ASSERT_TRUE(value == remainder);
+          ASSERT_TRUE(value.GetValue() == remainder);
+        }
+        {
+          auto value = first + ModularType(second);
+          ASSERT_TRUE(value.IsValid());
+          ASSERT_TRUE(value == remainder);
+          ASSERT_TRUE(value.GetValue() == remainder);
+        }
+        {
+          auto value = ModularType(first) + ModularType(second);
+          ASSERT_TRUE(value.IsValid());
+          ASSERT_TRUE(value == remainder);
+          ASSERT_TRUE(value.GetValue() == remainder);
+        }
+      }
+    }
+    ModularType value = 0;
+    i64 remainder = 0;
+    for (i64 add = MinValue; add <= MaxValue; ++add) {
+      value += add;
+      remainder = (((remainder + add) % MOD) + MOD) % MOD;
+      ASSERT_TRUE(value.IsValid());
+      ASSERT_TRUE(value == remainder);
+      ASSERT_TRUE(value.GetValue() == remainder);
+    }
+  }
+
+  void TestSubstraction() {
+    for (i64 first = MinValue; first <= MaxValue; ++first) {
+      for (i64 second = MinValue; second <= MaxValue; ++second) {
+        auto remainder = (((first - second) % MOD) + MOD) % MOD;
+        {
+          auto value = ModularType(first) - second;
+          ASSERT_TRUE(value.IsValid());
+          ASSERT_TRUE(value == remainder);
+          ASSERT_TRUE(value.GetValue() == remainder);
+        }
+        {
+          auto value = first - ModularType(second);
+          ASSERT_TRUE(value.IsValid());
+          ASSERT_TRUE(value == remainder);
+          ASSERT_TRUE(value.GetValue() == remainder);
+        }
+        {
+          auto value = ModularType(first) - ModularType(second);
+          ASSERT_TRUE(value.IsValid());
+          ASSERT_TRUE(value == remainder);
+          ASSERT_TRUE(value.GetValue() == remainder);
+        }
+      }
+    }
+
+    ModularType value = 0;
+    i64 remainder = 0;
+    for (i64 sub = MinValue; sub <= MaxValue; ++sub) {
+      value -= sub;
+      remainder = (((remainder - sub) % MOD) + MOD) % MOD;
+      ASSERT_TRUE(value.IsValid());
+      ASSERT_TRUE(value == remainder);
+      ASSERT_TRUE(value.GetValue() == remainder);
+    }
+  }
+
+  void TestMultiplication() {
+    for (i64 first = MinValue; first <= MaxValue; ++first) {
+      for (i64 second = MinValue; second <= MaxValue; ++second) {
+        auto remainder = (((first * second) % MOD) + MOD) % MOD;
+        {
+          auto value = ModularType(first) * second;
+          ASSERT_TRUE(value.IsValid());
+          ASSERT_TRUE(value == remainder);
+          ASSERT_TRUE(value.GetValue() == remainder);
+        }
+        {
+          auto value = first * ModularType(second);
+          ASSERT_TRUE(value.IsValid());
+          ASSERT_TRUE(value == remainder);
+          ASSERT_TRUE(value.GetValue() == remainder);
+        }
+        {
+          auto value = ModularType(first) * ModularType(second);
+          ASSERT_TRUE(value.IsValid());
+          ASSERT_TRUE(value == remainder);
+          ASSERT_TRUE(value.GetValue() == remainder);
+        }
+      }
+    }
+
+    ModularType value = 0;
+    i64 remainder = 0;
+    for (i64 mul = MinValue; mul <= MaxValue; ++mul) {
+      value *= mul;
+      remainder = (((remainder * mul) % MOD) + MOD) % MOD;
+      ASSERT_TRUE(value.IsValid());
+      ASSERT_TRUE(value == remainder);
+      ASSERT_TRUE(value.GetValue() == remainder);
+    }
+  }
+
+  void Test() {
+    TestCreation();
+    TestAddition();
+    TestSubstraction();
+    TestMultiplication();
+  }
+};
+
+void Test64() {
+  Tester<i64, 1>().Test();
+  Tester<i64, 2>().Test();
+  Tester<i64, 3>().Test();
+  Tester<i64, 4>().Test();
+  Tester<i64, 5>().Test();
+  Tester<i64, 6>().Test();
+  Tester<i64, 7>().Test();
+  Tester<i64, 8>().Test();
+  Tester<i64, 9>().Test();
+  Tester<i64, 10>().Test();
+}
+
+void Test32() {
+  Tester<i32, 13>().Test();
+}
+
+void Test16() {
+  Tester<i16, 17>().Test();
+}
+
+void Test8() {
+  Tester<i8, 37>().Test();
+}
+
+int main() {
+  Test8();
+  Test16();
+  Test32();
+  Test64();
+}
