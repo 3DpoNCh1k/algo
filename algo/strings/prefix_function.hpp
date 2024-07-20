31b97445a97ff9c57330a6252d1817c2023b3ab4
Polyashov Maxim <polyashovmaxim@gmail.com>
2024-07-20 15:24:43 +0300

    refactor prefix function

--- algo/strings/prefix_function.hpp
+++ algo/strings/prefix_function.hpp
@@ -4,22 +4,28 @@
 #include <vector>
 
 namespace algo::strings {
+/*
+For every prefix `p` of `s` calculates the length of the longest string `t` that
+1) `t` != `p`
+2) `t` is a prefix of `p`
+3) `t` is a suffix of `p`
+*/
 std::vector<int> PrefixFunction(const std::string& s) {
   int n = s.size();
-  std::vector<int> pref(n);
+  std::vector<int> length(n);
   for (int i = 1; i < n; ++i) {
-    int p = pref[i - 1];
+    int previous_length = length[i - 1];
     while (true) {
-      if (s[i] == s[p]) {
-        pref[i] = p + 1;
+      if (s[i] == s[previous_length]) {
+        length[i] = previous_length + 1;
         break;
       }
-      if (p == 0) {
+      if (previous_length == 0) {
         break;
       }
-      p = pref[p - 1];
+      previous_length = length[previous_length - 1];
     }
   }
-  return pref;
+  return length;
 }
 }  // namespace algo::strings
--- tests/strings/prefix_function/solution.cpp
+++ tests/strings/prefix_function/solution.cpp
@@ -7,9 +7,9 @@ int main() {
   std::string s;
   std::cin >> s;
 
-  auto values = algo::strings::PrefixFunction(s);
-  for (int i = 0; i < values.size(); ++i) {
-    std::cout << values[i] << (i + 1 == values.size() ? "" : " ");
+  auto length = algo::strings::PrefixFunction(s);
+  for (int i = 0; i < length.size(); ++i) {
+    std::cout << length[i] << (i + 1 == length.size() ? "" : " ");
   }
   std::cout << "\n";
 }
